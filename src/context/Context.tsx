175e14b317444b939af8194c13f0ff08d7a66a09
omar <yourname@example.com>
2024-04-16 17:20:26 +0300

    add typing data effect

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -11,7 +11,11 @@ const ContextProvider = (props) => {
   const [loading, setLoading] = useState(false);
   const [resultData, setResultData] = useState("");
 
-  const delayPara = (index, nextWord) => {};
+  const delayPara = (index: number, nextWord: string) => {
+    setTimeout(() => {
+      setResultData((prev) => prev + nextWord);
+    }, 20 * index);
+  };
 
   const onSent = async (prompt: string) => {
     setResultData("");
@@ -21,19 +25,30 @@ const ContextProvider = (props) => {
     const response = await runChat(input);
     const responseArray = response.split("**");
     let newResponse = "";
+
     for (let i = 0; i < responseArray.length; i++) {
       if (i === responseArray.length - 1 && responseArray[i] === "") {
         // Skip if the last element is empty
         continue;
       }
-      if (i === 0 || i % 2 !== 1) {
+      if (i === 0 || i % 2 === 0) {
         newResponse += responseArray[i];
       } else {
         newResponse += "<b>" + responseArray[i] + "</b>";
       }
     }
-    const newResponse2 = newResponse.split("*").join("</br>");
-    setResultData(newResponse2);
+
+    // Split by "*" and join with <br> for line breaks
+    const newResponse2 = newResponse.split("*").join("<br>");
+
+    // Split by " " to process each word
+    const newResponseArray = newResponse2.split(" ");
+
+    for (let i = 0; i < newResponseArray.length; i++) {
+      const nextWord = newResponseArray[i];
+      delayPara(i, nextWord + " ");
+    }
+
     setLoading(false);
     setInput("");
   };
