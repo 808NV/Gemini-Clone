469dda76206a487609c29730a98feadf6e7cf511
omar <yourname@example.com>
2024-04-16 14:46:53 +0300

    modified

--- src/config/gemini.ts
+++ src/config/gemini.ts
@@ -49,6 +49,7 @@ async function runChat(prompt: string) {
   const result = await chat.sendMessage(prompt);
   const response = result.response;
   console.log(response.text());
+  return response.text();
 }
 
 export default runChat;
